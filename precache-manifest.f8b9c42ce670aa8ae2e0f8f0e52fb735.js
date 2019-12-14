d573cb39b8be230ddcefaa61f93d2d12792aa0e7
Warzecha <damian.warzecha1@gmail.com>
2019-12-14 17:14:31 +0100

    Updates

---
+++ precache-manifest.f8b9c42ce670aa8ae2e0f8f0e52fb735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8705b32c0645163a88f59d6bbe357d1",
+    "url": "/log-panda-frontend/index.html"
+  },
+  {
+    "revision": "16233468a4374d0612f3",
+    "url": "/log-panda-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc8388d42e38a321e48d",
+    "url": "/log-panda-frontend/static/js/2.0c45ca2b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/log-panda-frontend/static/js/2.0c45ca2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16233468a4374d0612f3",
+    "url": "/log-panda-frontend/static/js/main.f138978f.chunk.js"
+  },
+  {
+    "revision": "0fc7ffd24c23dccbe6e2",
+    "url": "/log-panda-frontend/static/js/runtime-main.5d2c515c.js"
+  }
+]);
